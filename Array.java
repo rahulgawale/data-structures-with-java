c902159c3e506d169e20c1c183bf33a87f6735e9
Rahul Gawale <rahul.g.jack@gmail.com>
2021-07-20 20:56:37 +0530

    first commit

---
+++ Array.java
@@ -0,0 +1,134 @@
+
+/* Dynamic Array Implementation Using Java */
+import java.util.Iterator;
+
+@SuppressWarnings("unchecked")
+public class Array<T> implements Iterable<T> {
+    private T[] array;
+    private int length = 0;
+    private int capacity = 0;
+
+    public int size() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public T get(int index) {
+        if (index >= length) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        return array[index];
+    }
+
+    public void set(T element, int index) {
+        if (index >= length) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        array[index] = element;
+    }
+
+    public void clear() {
+        for (int i = 0; i < length; i++) {
+            array[i] = null;
+        }
+        length = 0;
+    }
+
+    public Array() {
+        this(16);
+    }
+
+    public Array(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Capacity must be greater than or equals to 0.");
+        }
+        this.capacity = capacity;
+        array = (T[]) new Object[capacity];
+        System.out.println("array length: " + array.length);
+    }
+
+    public void add(T element) {
+        if (length + 1 >= capacity) {
+            if (capacity == 0) {
+                capacity = 1;
+            } else {
+                capacity = capacity * 2;
+            }
+
+            T[] newArray = (T[]) new Object[capacity];
+            for (int i = 0; i < length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+        array[length++] = element;
+    }
+
+    public T removeAt(int index) {
+        if (index >= length) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
+        T element = array[index];
+        array[index] = null;
+
+        for (int i = index; i < length; i++) {
+            array[i] = array[i + 1];
+        }
+        length--;
+        return element;
+    }
+
+    public int indexOf(T element) {
+        for (int i = 0; i < length; i++) {
+            if (array[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean remove(T element) {
+        int index = indexOf(element);
+        if (index == -1) {
+            return false;
+        }
+        removeAt(index);
+        return true;
+    }
+
+    public boolean contains(T element) {
+        return indexOf(element) != -1;
+    }
+
+    @Override
+    public String toString() {
+        if (length == 0) {
+            return "[]";
+        }
+        String value = "[";
+        for (int i = 0; i < length - 1; i++) {
+            value += array[i] + ", ";
+        }
+        value += array[length - 1] + "]";
+        return value;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<T>() {
+            int index = 0;
+
+            public boolean hasNext() {
+                return index < length;
+            }
+
+            public T next() {
+                return array[index++];
+            }
+        };
+    }
+}
---
+++ ArrayExample.java
@@ -0,0 +1,37 @@
+public class ArrayExample {
+
+    public static void main(String argv[]) {
+        System.out.println("First java program!");
+        Array<Integer> array = new Array<Integer>(3);
+        array.add(10);
+        array.add(20);
+        array.add(30);
+        array.add(40);
+
+        System.out.println("index of 10: " + array.indexOf(10));
+        System.out.println("removed at index 2: " + array.removeAt(2));
+
+        // below line throws index out of bound exception
+        // System.out.println("removed at index 2: " + array.removeAt(5));
+
+        System.out.println("index of 30: " + array.indexOf(30)); // -1
+        System.out.println("index of 40: " + array.indexOf(40)); // 3
+        array.add(50);
+        System.out.println("removed 50: " + array.remove(50)); // true
+        System.out.println("removed 50: " + array.contains(50)); // false
+        System.out.println("removed 10: " + array.contains(10)); // true
+        System.out.println("toString: " + array.toString()); // [10, 20, 40]
+        System.out.println("size of an array: " + array.size()); // 3
+        array.set(1, 2);
+        System.out.println("size of an array: " + array.size()); // 3
+        System.out.println("toString: " + array.toString()); // [10, 20, 40]
+        System.out.println("get element at 0: " + array.get(array.size() - 1)); // 1
+
+        for (Integer num : array) {
+            System.out.println("num " + num);
+        }
+
+        array.clear();
+        System.out.println("toString: " + array.toString()); // []
+    }
+}
